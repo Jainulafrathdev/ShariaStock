b57c2875da42607b45370d457ae9270735358c35
Jmafrath001 <jainulafrath05@gmail.com>
2024-11-05 16:26:00 +0530

    after connection change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/profileDB', {
+        await mongoose.connect('mongodb://52.201.237.41:27017/profileDB', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
